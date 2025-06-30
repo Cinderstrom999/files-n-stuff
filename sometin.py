59ed9607f629e2a9e95841d14166aa9dce5c0437
Cinderstrom999 <grahamjkohel@gmail.com>
2025-06-30 17:24:48 +0000

    the pain quizzz

---
+++ sometin.py
@@ -0,0 +1,51 @@
+
+q1=input ("what is 1234 x 12 ")
+if q1!= "14808":
+    while True:
+        print ("UNRIGHT")
+q2=input ("e= what, hinht: think light bulb guy ")
+if q2!= "MC SQUARED":
+    while True:
+            print ("UNRIGHT")
+q3=input ("please enter your credit card number ")
+if q3!= "WHAT NO":
+    while True:
+         print ("UNRIGHT")
+q4=input ("op[e]n the [hyperlinked blocked] 4 the fire4orks ")
+if q4!= "snowgrave" and q4!= "spamton": 
+    while True:
+            print ("UNRIGHT")
+BIGSHOT=input ("nows your chnce to bee a what? ")
+if BIGSHOT!= "BIG SHOT":
+    while True:
+            print ("UNRIGHT")
+C=input ("OPEn tHYE oCEAn Ore tHE doOr wiLl nOT clOSE ")
+if C!= "?":
+    while True:
+         print ("UNRIGHT")
+q5=input ("what do thy be blocked ")
+if q5!= "HYPERLINKED":
+    while True:
+            print ("UNRIGHT")
+crule=input ("2 anwers this time HAhhaaAAAhhhhAAHhhaAahAahAHaAHAahA ")
+List=crule.split(", ")
+if "2" not in List or "anwers"not in List:
+    while True:
+            print ("UNRIGHT")  
+
+Q6=input  ("what is the name of this camp ")
+if Q6!= "summertech":
+        while True:
+            print ("UNRIGHT")
+e1=input (" what do thy bee with unright this word? fone ")
+if e1!= "f not ph" and e1!= "F not PH":
+     while True:
+        print ("UNRIGHT")
+E3=input  ("what is this key ")
+if E3!= "space" or E3!= "space key":
+    while True:
+        print ("UNRIGHT")
+else: 
+  while True:
+     print ("correct u win yayayayayayayyayayayayayayayayayyaayy ")
+    
