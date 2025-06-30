0b0ce1445343a58b4f887e0a6418a0ef211096bb
Cinderstrom999 <grahamjkohel@gmail.com>
2025-06-30 13:56:35 +0000

    mad done

--- somename.py
+++ somename.py
@@ -4,11 +4,11 @@ part1 =input ("hello " +name+ " please enter a word that makes sense in the cont
 part2 =input (part1+ " wants ")
 part3 =input ( part2 + " ok. he gets the " + part2 + " with a ")
 part4 =input ("but when he was trying to stop a")
-part5 =input ("the "+ part4 + "did" )
+part5 =input ("the "+ part4  )
 part6 =input (" so the king")
-parte =input (" and the king lived in")
-partefr =input ("forever. would youy like to hear yoor full story? ")
-idcs =input ("i do not care you are hearing it anyways L. once there was a king named " +part1+ "wants " +part2+ "he gets the " +part2 +"with a " +part3+ "but when he was trying to stop a " +part4+ "the " +part1+ "did" +part5+ "" )
+parte =input (" and the king lived ")
+partefr =input ("forever. would you like to hear your full story? ")
+idcs =input ("i do not care you are hearing it anyways L. once there was a king named " +part1+ " wants " +part2+ "  he gets the " +part2 +" with a " +part3+ " but when he was trying to stop a " +part4+ " the " +part1+ " did" +part5+ " so the king " +part6+ "and  forever lived lived in " +parte )
 
 
 
