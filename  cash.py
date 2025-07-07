1dcc397da82f94deb958511c9ad37a5414a95155
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-07 13:57:59 +0000

    more gambaling

---  cash.py
+++  cash.py
@@ -1,4 +1,6 @@
+import random 
 
+print(":D")
 bellagio=[]
 for i in range (10):
     for x in range (4):
@@ -25,12 +27,37 @@ for i in range (10):
                 elif x==3:
                     card=neutrals[z]+" of ROARING NIGHT (pathetic) raaaaaaaaaaaaar"
                 bellagio.append(card)
-bellagio.append("instint win WOW great job thats almost a 2% chance good job")
+bellagio.append("joker")
 print (bellagio)
 
+bellagio.append("oceans joker")
+
+
+
 round = False
+dracsplayed=[]
+player1 = []
+player2 = []
+bank = 0
+player1bank = 500
+player2bank = 555
+richboibank = 999
+
 while True:
-    player1 = []
-    player2 = []
-    
+    if round==False:
+
+        print ("player 1 turn XD")
+        drac=bellagio[random.randint(0,48)]
+        player1.append(drac)
+        dracsplayed.append(drac)
+        print("u drew "+drac + " sounds nice")
+        if drac == "joker":
+            print("congr]tasg[s [yiot]iuu w[ion]")
+            player1bank+=bank
+        elif drac == "oceans joker":
+            print("u lose, remember kids 99.9999999999999% of gambelers quit befoere they win big!!!")
+            richboibank+=bank
+        
+
+
 
