fa9dd75604cfacb31cb13f7cd79e9dc24746f4c1
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-10 15:09:31 +0000

    clock

---
+++ clocktimer.py
@@ -0,0 +1,55 @@
+import time 
+Dface= input ("timer or stopwatch ")
+if Dface=="stopwatch":
+    t=0
+    while True:
+        t=t+.1
+        time.sleep(.1)
+        print (t)
+
+elif Dface=="SUPER SECRET":
+     while True:
+        print ("japan is at") 
+        print (time.ctime(time.time()+32400))
+        time.sleep(1)
+
+
+elif Dface=="not so super":
+    while True:
+        print ("L.A. is ")
+        print(time.ctime(time.time()-25200))
+        time.sleep(1)
+
+
+elif Dface== "z4a3qx2s1":
+    print ("welcome agent our TIME  here is short, i hope you fined this helpful in your investigation   signed -agent ⚒ ")
+    time.sleep(6)
+    while True:
+        print (time.ctime(time.time()+10800))
+        time.sleep(1)
+
+
+
+elif Dface== ":|":
+    print ("welcome :D agent here is your misson in code >:O D:< O: /: <: =D =P :P :O |: C= XD :( :V :Y =F => i wish u luck agent D: out")
+
+
+elif Dface== "normal":
+    while True:
+        print (time.ctime(time.time()-14400))
+        time.sleep(1)
+
+elif Dface== "timer":
+    qut = int(input ("how many seconds?"))
+    t=0
+    while True:
+        t=t+.1
+        time.sleep(.1)
+        print (t)
+        if t>=qut:
+            while True:
+                print ("alarrrrrrrm allllllllllarm") 
+    
+        
+
+
