afa4d10c8ef23557d9ff42e8fc16c50005f5f325
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-10 15:15:29 +0000

    dfgtb

--- clocktimer.py
+++ clocktimer.py
@@ -8,8 +8,8 @@ if Dface=="stopwatch":
         print (t)
 
 elif Dface=="SUPER SECRET":
+    print ("japan is at") 
      while True:
-        print ("japan is at") 
         print (time.ctime(time.time()+32400))
         time.sleep(1)
 
@@ -30,7 +30,7 @@ elif Dface== "z4a3qx2s1":
 
 
 
-elif Dface== ":|":
+elif Dface== "xd":
     print ("welcome :D agent here is your misson in code >:O D:< O: /: <: =D =P :P :O |: C= XD :( :V :Y =F => i wish u luck agent D: out")
 
 
