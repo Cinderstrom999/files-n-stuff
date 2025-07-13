ac6bd360c09cfd6e6d912663e6fdf79c50b5fe41
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-13 20:52:08 +0000

    zgr

---
+++ z4a3qx2s1.py
@@ -0,0 +1,20 @@
+import random 
+
+
+runstart=input("hi this is billy shears the 7 year olds first project i hope yoh like it by the way what is your name? ")
+
+if runstart=="mike oscar":
+    rpdoor=input("ah [sigh] i see well protocall active. rock paper sissors go ")
+    door=random.randint (1,4)
+    if door==(1) and rpdoor==(paper):
+        print ("nice, now go again, also do it better this time!!!")
+    if door==(2) and rpdoor==(1):
+        print ("zxgzsdzdzxfasternexttimehasrvnpasdbpa")
+    
+
+if runstart==":D the :|":
+    print (":D :| :] :( ) =D D= {=!!! =V")
+
+
+else:
+    print ("oh hi " +runstart + " wellcome! yes i can take prior knowlege imprest family ha ha!!! also watch this...")
