2acf979a2f3c8f70624d9c65dca45c2147033e2c
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-14 13:47:03 +0000

    hjfgxcdm

--- z4a3qx2s1.py
+++ z4a3qx2s1.py
@@ -9,16 +9,29 @@ import random
 runstart=input("hi this is billy shears the 7 year olds first project i hope yoh like it by the way what is your name? ")
 
 if runstart=="mike oscar":
-    rpdoor=input("ah [sigh] i see well protocall active. rock paper sissors go ")
-    door=random.randint (1,4)
-    if door==(1) and rpdoor==(paper):
-        print ("nice, now go again, also do it better this time!!!")
-    if door==(2) and rpdoor==(1):
-        print ("zxgzsdzdzxfasternexttimehasrvnpasdbpa")
+    while True:
+        rpdoor=input("ah [sigh] i see well protocall active. rock paper sissors go ")
+        door=random.randint (1,4)
+        if door==(1) and rpdoor==("paper"):
+            print ("nice, now go again, also do it better this time!!!")
+        if door==(2) and rpdoor==("scissors"):
+            print ("zxgzsdzdzxfasternexttimehasrvnpasdbpa")
+        if door==(3) and rpdoor==("rock"):
+            print ("you lose go agin")
+        else:
+            print ("toe")
     
 
 if runstart==":D the :|":
     print (":D :| :] :( ) =D D= {=!!! =V")
+    time.sleep(5)
+    while True:
+        print ("1q 2a 3z")
+        print ("i win ")
+        PRINT("HOLWOW HEART")
+
+if runstart=="big boi":
+    print ("ok type this https://www.google.com/search?q=depth+first+search+solution&sca_esv=05f7f9216b2ed83f&ei=Fgh1aNraPNPsptQPvqbHoAQ&oq=depth+first+search+soultio&gs_lp=Egxnd3Mtd2l6LXNlcnAiGmRlcHRoIGZpcnN0IHNlYXJjaCBzb3VsdGlvKgIIADIGEAAYFhgeMgYQABgWGB4yCBAAGAgYDRgeMgsQABiABBiGAxiKBTILEAAYgAQYhgMYigUyCxAAGIAEGIYDGIoFMggQABiABBiiBDIIEAAYgAQYogQyCBAAGIAEGKIEMggQABiiBBiJBUikV1DQBFjPSHABeAGQAQCYAVGgAZEEqgEBOLgBAcgBAPgBAZgCCaACugTCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBcICChAAGIAEGEMYigXCAgUQABiABMICCxAAGIAEGJECGIoFwgIFECEYoAGYAwCIBgGQBgqSBwE5oAfYMbIHATi4B7cEwgcFMC41LjTIBx8&sclient=gws-wiz-serp")
 
 
 else:
