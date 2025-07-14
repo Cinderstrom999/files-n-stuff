324d3ac140f5e100e7fbd053cd7ceae092f4adaa
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-14 15:10:13 +0000

    z

--- clocktimer.py
+++ clocktimer.py
@@ -52,4 +52,3 @@ elif Dface== "timer":
     
         
 
-
--- z4a3qx2s1.py
+++ z4a3qx2s1.py
@@ -34,6 +34,17 @@ if runstart=="big boi":
     print ("ok type this https://www.google.com/search?q=depth+first+search+solution&sca_esv=05f7f9216b2ed83f&ei=Fgh1aNraPNPsptQPvqbHoAQ&oq=depth+first+search+soultio&gs_lp=Egxnd3Mtd2l6LXNlcnAiGmRlcHRoIGZpcnN0IHNlYXJjaCBzb3VsdGlvKgIIADIGEAAYFhgeMgYQABgWGB4yCBAAGAgYDRgeMgsQABiABBiGAxiKBTILEAAYgAQYhgMYigUyCxAAGIAEGIYDGIoFMggQABiABBiiBDIIEAAYgAQYogQyCBAAGIAEGKIEMggQABiiBBiJBUikV1DQBFjPSHABeAGQAQCYAVGgAZEEqgEBOLgBAcgBAPgBAZgCCaACugTCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBcICChAAGIAEGEMYigXCAgUQABiABMICCxAAGIAEGJECGIoFwgIFECEYoAGYAwCIBgGQBgqSBwE5oAfYMbIHATi4B7cEwgcFMC41LjTIBx8&sclient=gws-wiz-serp")
 
 
+if runstart=="ship":
+    rig=input("1 ore 2 ")
+    if rig=="1":
+        print ("2 bad it 2")
+    else:
+        print ("2 bad it was 1")
+    
+
+
+
+
 else:
     print ("oh hi " +runstart + " wellcome! yes i can take prior knowlege imprest family ha ha!!! also watch this...")  
     time.sleep(7)
