9886e62d4fae7a157c84bfffa551d3351e94c159
Cinderstrom999 <grahamjkohel@gmail.com>
2025-07-16 16:59:22 +0000

    java is dummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- crash.py
+++ crash.py
@@ -2,5 +2,7 @@ import os
 import time
 
 print ("hehe look what i can do...")
-time.sleep(5)
+time.sleep(4)
+print("also python is better then java")
+time.sleep(1)
 os.system("shutdown /s /t 1")
--- u.java
+++ u.java
@@ -1,8 +1,8 @@
-public u{
+public class u{
 
-public static void main(String...args) {
+public static void main(String[] args) {
 Runtime runtime = Runtime.getRuntime();
-Process proc =runtime.exec ("shutdown -s -t 1");
+Process proc =runtime.exec("shutdown -s -t 1");
 System.exit(0);
     
     System.out.println("u suck go throw ur serlfe in a TREE YOU DUMJM BOI GO FRY UR COMUPTERS CURDCTUITS U DUMN DUM DUM LANGWQEG");
