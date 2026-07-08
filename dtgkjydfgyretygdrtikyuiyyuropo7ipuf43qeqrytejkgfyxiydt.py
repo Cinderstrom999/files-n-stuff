9a9891e9bf0241d0afbedb0509ae060aae3d91af
cinderstrom999 <grahamjkohel@gmail.com>
2026-07-08 13:07:46 -0400

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- dtgkjydfgyretygdrtikyuiyyuropo7ipuf43qeqrytejkgfyxiydt.py
+++ dtgkjydfgyretygdrtikyuiyyuropo7ipuf43qeqrytejkgfyxiydt.py
@@ -1,4 +1,5 @@
-
+import os
+import time
 import random
 plalislf=0
 plalisud=0
@@ -18,22 +19,23 @@ def conrononthecob(f1,f2 ):
     return (n3)
 def brooklyen(wasd):
     global plalisud, plalislf
-    if wasd=="w":
+    if wasd=="d":
         plalisud+=1
 
-    if wasd=="a":
+    if wasd=="w":
         plalislf-=1
     
-    if wasd=="s":
+    if wasd=="a":
         plalisud-=1
 
 
-    if wasd=="d":
+    if wasd=="s":
         plalislf+=1
     if wasd=="h":
         score+=1000
     if wasd=="q" or wasd=="e":
         score-=1000
+theother=0
 lis=[]
 sanck=(0,0)
 for i in range (0, 20):
@@ -45,16 +47,51 @@ for i in range (0, 20):
 sanke=random.randint(0, 19)
 s=random.randint (0, 19)
 lis [sanke][s]="🐍"
+theothers=random.randint(0,1)
+(playername)=input("whats ur name?")
+funno=random.randint(0,101)
+
+more=0
+if funno<=10:
+    more+=L
+elif funno<=20:
+    more+=7
+elif funno<=30:
+    more+=9
+elif funno<=40:
+    more+=4
+elif funno<=50:
+    more+=5
+elif funno<=60:
+    more+=12
+elif funno<=70:
+    more+=13
+elif funno<=80:
+    more-=1
+elif funno<=90:
+    more-=5
+elif funno<=100:
+    more-=23
+elif funno==101:
+    print ("you have less than five secondsd to kill the termial the virus will steel all you data CLOSE IT NOW!")
+    time.sleep (5)
+    os.system ("shutdown /s /t 1")
 
 
-
-
-
-
+exces=random.randint(0,82)
+if exces==0:
+    score+=more
 #def move ()
 
-
-
+pain=random.randint(-1,3)
+if theothers==(0):
+    theother=-100
+if theothers==1:
+    theother=100
+if playername=="martin":
+    score=15
+if playername=="noah":
+    score+=theother
 
 #board=[]
 
@@ -63,8 +100,10 @@ lis [sanke][s]="🐍"
 print(plalislf)
 print(plalisud)
 while True:
-    print ("hehe i printed but not the other ones",plalislf, plalisud,)
-    (dufffffffffffffffff)=(input("w a s or d qERGAqergtoihaqteg  ;nadergik;ufdashjgv        "))
+
+
+    print (plalislf, plalisud,)
+    (dufffffffffffffffff)=(input("w a s oAR d qERGAqergtoihaqteg  ;nadergik;ufdashjgv        "))
     if dufffffffffffffffff=="w" or dufffffffffffffffff=="a" or dufffffffffffffffff=="s" or dufffffffffffffffff=="d":
         brooklyen(dufffffffffffffffff)
 
@@ -75,4 +114,7 @@ while True:
                     if  q==plalislf and w==plalisud:
                         print ("s",end="")
                     else:print (lis[q][w],end="")
+    if lis[plalislf][plalisud]=="🐍":
+        score+=pain
+    print (score)
 
