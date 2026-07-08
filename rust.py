415adba1c0992258942d040c978f6663b346f2cc
cinderstrom999 <grahamjkohel@gmail.com>
2026-07-08 10:01:01 -0400

    commit

---
+++ rust.py
@@ -0,0 +1,8 @@
+lis=[]
+for y in range (5):
+    temp=[]
+    for x in range (5): 
+        temp.append("_")
+    lis.append(temp)
+
+
---
+++ testingh.py
@@ -0,0 +1,36 @@
+import time
+
+
+lis=[2, 5, 6, 3, 1, 0, 4, 7,]
+alt=[0, 1, 2, 3, 4, 5, 6, 7,]
+
+(lisasimpson)=int(input("tell me a numbert 0-7 and i will tel you is rnaki8n g in the datd base sorry for yepoys we dont hjave a spell checker"))
+something=-1
+if lisasimpson==9:
+    time.sleep (30.1)
+    print("why did you just stare at me for 30 seconds ")
+elif lisasimpson>=9:
+    print ("thats")
+    time.sleep(4)
+    print ("too high ")
+    time.sleep (3)
+    print ("well you aked for it ")
+    import os 
+    os.system ("shutdown /s /t 1")
+for i in lis:
+    if i==lisasimpson:
+        something=i
+        time.sleep (6.34) 
+        print ("alrighyt let me go find it ")
+        time.sleep (10.903)
+        print ("found the file let me look")
+        time.sleep (4.2576)
+        print ("*flips page *")
+        time.sleep (4.2576)
+        print ("*flips page *")
+        time.sleep (4.2576)
+        print ("*flips page *")
+        time.sleep (7)
+        print ("found it ")
+        print (i)
+
